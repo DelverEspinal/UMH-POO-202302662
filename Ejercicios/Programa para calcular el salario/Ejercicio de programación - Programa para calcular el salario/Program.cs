ed1490385c6be9a48acdf578c6c809a4c4d839bf
DelverEspinal <delvervarela66@gmail.com>
2025-08-13 21:37:46 -0600

    Tarea 2.1 Ejercicio de Programacion

---
+++ Ejercicios/Programa para calcular el salario/Ejercicio de programación - Programa para calcular el salario/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_de_programación___Programa_para_calcular_el_salario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double CostoHora = 100;
+            double horasTrabajadas = 40;
+            double totalDevengado = CostoHora * horasTrabajadas;
+
+           Console.WriteLine("Las horas que se trabajaron en la semana son: " + horasTrabajadas );
+
+           Console.WriteLine("La hora se paga en: L. " + CostoHora );
+
+           Console.WriteLine("El salario semanal es: L. " + totalDevengado );
+
+            Console.ReadLine();
+        }
+    }
+}
